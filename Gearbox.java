2651ce989165e592270b8592877d73d1c3b8a80c
Moctar <mockhtar@gmail.com>
2020-01-21 00:38:17 +0100

    Utilisation des constantes

--- Gearbox.java
+++ Gearbox.java
@@ -2,24 +2,35 @@ package electrics.industries;
 
 public class Gearbox {
 
+	private final static int decalageSuperieur = 2000;
+	private final static int decalageInferieur = 500;
+	private final static int pointMort = 0;
+	private final static int nombreVitesseMax = 6;
+	private final static int premierVitesse = 1;
+	
+
     private int s = 0;
     private int e = 0;
 
     public void doit(int i) {
-        if (s < 0) {
+        if (s < pointMort) {
             // do nothing!
             e = i;
         }
         else {
-            if (s > 0) {
-                if (i > 2000) {
+        	
+            if (s > pointMort) {
+                if (i > decalageSuperieur) {
                     s++;
-                } else if (i < 500) {
+                } else if (i < decalageInferieur) {
                     s--;
                 }
-            } if (s > 6) {
+            }
+            
+            if (s > nombreVitesseMax) {
                 s--;
-            } else if (s < 1) {
+            } 
+            else if (s < premierVitesse) {
                 s++;
             }
             e = i;
