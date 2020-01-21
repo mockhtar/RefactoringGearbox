3f911ef0ed36546988245556e98a9ec54b4f0591
Moctar <mockhtar@gmail.com>
2020-01-21 02:07:35 +0100

    Refactoring + commentaires

--- Gearbox.java
+++ Gearbox.java
@@ -7,34 +7,47 @@ public class Gearbox {
 	private final static int pointMort = 0;
 	private final static int nombreVitesseMax = 6;
 	private final static int premierVitesse = 1;
-	
 
 	private int vitesse = 0;
-    private int regime = 0;
+	private int regime = 0;
 
-    public void doit(int regimeactuelle) {
-        if (vitesse < pointMort) {
-            // do nothing!
-        	regime = regimeactuelle;
-        }
-        else {
-        	
-            if (vitesse > pointMort) {
-                if (regimeactuelle > decalageSuperieur) {
-                	vitesse++;
-                } else if (regimeactuelle < decalageInferieur) {
-                	vitesse--;
-                }
-            }
-            
-            if (vitesse > nombreVitesseMax) {
-            	vitesse--;
-            } 
-            else if (vitesse < premierVitesse) {
-            	vitesse++;
-            }
-            regime = regimeactuelle;
-        }
-    }
+	public void doit(int regimeactuelle) {
+		regime = regimeactuelle;
+		if(vitesseAIncrementer()){
+			incrementVitesse();
+		}
+		else if(vitesseADecrementer()){
+			retrograderVitesse();
+		}
+
+	}
+	/*
+	* Methode pour faire passer la boite à la vitesse superieur
+	*/
+	public void incrementVitesse(){
+		vitesse++;
+	}
+
+	/*
+	* Methode pour faire rétrograder la boite de vitesse
+	*/
+	public void retrograderVitesse(){
+		vitesse--;
+	}
+
+	/*
+	* Veirfie s'il faut incrementer la boite de vitesse
+	*/
+	public boolean vitesseAIncrementer() {
+		return ((vitesse > pointMort && regime > decalageSuperieur) || (vitesse < premierVitesse));
+	}
+
+	/*
+	* Verifie s'il faut rétrograder la boite de vitesse
+	*/
+	public boolean vitesseADecrementer() {
+		return ((vitesse > pointMort && regime < decalageInferieur) || vitesse > nombreVitesseMax);
+	} }
+	}
 
 }
